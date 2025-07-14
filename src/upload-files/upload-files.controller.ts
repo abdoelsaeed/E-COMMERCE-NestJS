9adce713025f6bd9fd00e3737e864955e703fb02
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-07-15 01:20:42 +0300

    add validation to upload images

--- src/upload-files/upload-files.controller.ts
+++ src/upload-files/upload-files.controller.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment *//* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { CloudinaryService } from './upload-files.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
@@ -11,14 +11,37 @@ export class UploadFilesController {
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+  async uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 50000,
+            message: `File is to large must be less than 50kbs`,
+          }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     const image = await this.cloudinaryService.uploadFile(file);
     const url = image.url;
-    return {url};
+    return { url };
   }
   @Post('uploads')
   @UseInterceptors(FilesInterceptor('file', 5))
-  async uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+  async uploadImages(
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    files: Express.Multer.File[],
+  ) {
     const images = await Promise.all(
       files.map(async (file) => {
         const result = await this.cloudinaryService.uploadFile(file);
