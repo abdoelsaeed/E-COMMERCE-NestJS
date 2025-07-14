6ffb21a87a03af7dce9161724e23c01901abb9a1
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-07-15 02:08:38 +0300

    add validate upload

--- src/upload-files/upload-files.controller.ts
+++ src/upload-files/upload-files.controller.ts
@@ -1,15 +1,21 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment *//* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CloudinaryService } from './upload-files.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-
+import { RateLimit } from 'nestjs-rate-limiter';
+import { AuthGuard } from 'src/guard/Auth.guard';
+import { Roles } from 'src/guard/user.decorator';
 @Controller('image')
 export class UploadFilesController {
   constructor(private readonly cloudinaryService: CloudinaryService) {}
 
   @Post('upload')
+  @UseGuards(AuthGuard)
+  @Roles(['Admin', 'User'])
+  @RateLimit({ points: 3, duration: 3600 })
   @UseInterceptors(FileInterceptor('file'))
   async uploadImage(
     @UploadedFile(
@@ -19,7 +25,7 @@ export class UploadFilesController {
             maxSize: 50000,
             message: `File is to large must be less than 50kbs`,
           }),
-          new FileTypeValidator({ fileType: 'image/jpeg' }),
+          new FileTypeValidator({ fileType: 'image/(jpeg|png|webp)' }),
         ],
       }),
     )
@@ -29,14 +35,17 @@ export class UploadFilesController {
     const url = image.url;
     return { url };
   }
+
   @Post('uploads')
+  @UseGuards(AuthGuard)
+  @Roles(['Admin', 'User'])
   @UseInterceptors(FilesInterceptor('file', 5))
   async uploadImages(
     @UploadedFiles(
       new ParseFilePipe({
         validators: [
           new MaxFileSizeValidator({ maxSize: 500000 }),
-          new FileTypeValidator({ fileType: 'image/jpeg' }),
+          new FileTypeValidator({ fileType: 'image/(jpeg|png|webp)' }),
         ],
       }),
     )
